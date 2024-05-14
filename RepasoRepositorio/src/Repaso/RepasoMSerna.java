6bc01769348832e5b461066636d1bfa14192dd37
mserna <mserna@LAP0041.gruposiglo.local>
2024-05-14 04:14:49 -0500

    Se actualiza 14/05/2024

--- RepasoRepositorio/src/Repaso/RepasoMSerna.java
+++ RepasoRepositorio/src/Repaso/RepasoMSerna.java
@@ -4,8 +4,8 @@ public class RepasoMSerna {
 	
 	public static void main(String[] arg) {
 		
-		System.out.println("Hola Leopoldo, esto es un Repasp Repositorio");
-		
+		System.out.println("Hola Leopoldo, esto es un Repaso Repositorio");
+		System.out.println("Actualizado 14/05/2024 ");
 	}
 
 }
